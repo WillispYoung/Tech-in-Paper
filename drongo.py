fd43a35476d4c183b3d9996f470d7720284f3a11
willisp <willispyoung@gmail.com>
2019-03-15 15:26:31 +0800

    initiate

---
+++ drongo.py
@@ -0,0 +1,8 @@
+"""
+    This document servers as an implementation of Drongo Proposed in
+    [Drongo: Speeding Up CDNs with Subnet Assimilation from the Client][CoNEXT'17].
+
+    Author: Willisp Young
+    StartDate: 2019-03-15
+"""
+
