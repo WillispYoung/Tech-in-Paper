98f62a07dc1500e12413e4746a54b0424a025d36
willisp <willispyoung@gmail.com>
2019-03-29 10:35:51 +0800

    init

--- drongo.py
+++ drongo.py
@@ -6,3 +6,28 @@
     StartDate: 2019-03-15
 """
 
+import dns.resolver
+
+resolver = dns.resolver.Resolver()
+resolver.nameservers = ['8.8.8.8', '8.8.4.4']
+
+answers = resolver.query("google.com", "A")
+for data in answers:
+    print data
+
+# baidu.com [with/without nameserver; China/Singapore]
+# 220.181.57.216 (Beijing, Haidian District)
+# 123.125.115.110 (Beijing, Xicheng District)
+
+# google.com
+# Singapore [without nameserver]
+# 172.217.194.100 (Virginia)
+# 172.217.194.138 (Singapore)
+# 172.217.194.113
+# 172.217.194.101
+# 172.217.194.102
+# 172.217.194.139
+# China [without nameserver]
+# 6.6.6.6 (fake)
+# China [with nameserver]
+# 172.217.161.174 (HK)
